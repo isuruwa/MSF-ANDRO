7da0f437f70f209c9faf929affc0c6ecdb58fc89
Devil Master <72663288+isuruwa@users.noreply.github.com>
2021-09-23 16:34:24 +0530

    Create banner.sh

---
+++ banner.sh
@@ -0,0 +1,46 @@
+cyan='\033[1;36m'
+green='\033[1;32m'
+red='\033[1;31m'
+yellow='\033[1;33m'
+blue='\033[1;34m'
+purple='\033[1;35m'
+reset='\033[0m'
+Escape="\033";
+RedF="${Escape}[31m";
+GreenF="${Escape}[32m";
+LighGreenF="${Escape}[92m"
+YellowF="${Escape}[33m";
+BlueF="${Escape}[34m";
+CyanF="${Escape}[36m";
+
+
+function banner(){
+
+echo -e " \e[1;37m   [\e[1;31m+\e[1;37m]|------------------------------------------|\e[1;37m[\e[1;31m+\e[1;37m]"
+echo -e " \e[1;37m   [\e[1;31m+\e[1;37m]|        $green DEVELOPED $red BY $green DEVIL MASTER      \e[1;37m|\e[1;37m[\e[1;31m+\e[1;37m]"
+echo -e " \e[1;37m   [\e[1;31m+\e[1;37m]|      $blue       github.com/isuruwa           \e[1;37m|\e[1;37m[\e[1;31m+\e[1;37m]"
+echo -e " \e[1;37m   [\e[1;31m+\e[1;37m]|             t.me/fsocietyadmn            |\e[1;37m[\e[1;31m+\e[1;37m]"
+echo -e " \e[1;37m   [\e[1;31m+\e[1;37m]|------------------------------------------|\e[1;37m[\e[1;31m+\e[1;37m]"
+echo ""
+echo ""
+
+}
+
+clear
+figlet -f smmono9 "   MSF-ANDRO" | lolcat
+
+echo -e "
+       \e[1;33m* \e[1;32m      *       ╱▔▔▔▔▔▔▔▔╲       *    \e[1;33m   * \e[1;32m
+       \e[1;31m    * \e[1;31m      * \e[1;32m  ▏\e[1;31m        \e[1;32m▕   \e[1;31m*      \e[1;31m * \e[1;32m
+  \e[1;31m*   \e[1;32m * \e[1;33m      *      \e[1;32m▕\e[1;31m ╭━╮\e[1;32m┈┈\e[1;31m╭━╮\e[1;32m▕     \e[1;33m  *      \e[1;32m * \e[1;31m   * \e[1;32m
+                       ╲\e[1;31m╰━╯\e[1;32m╱╲\e[1;31m╰━╯\e[1;32m╱ 
+        \e[1;32m   (@\e[1;33m_\e[1;31m \e[1;32m  \e[1;33m    \e[1;32m \e[1;32m  ▏╮┈▔▔┈╭▕ \e[1;31m  
+    \e[1;32m _     \e[1;33m ) \_\e[1;32m_____\e[1;31m____\e[1;31m┣╋╋╋╋┫\e[1;31m____\e[1;32m_______________
+   \e[1;32m (_)\e[31m@8@8\e[1;32m{}< \e[1;33m_\e[1;32m_____\e[1;31m________\e[1;31m______\e[1;32m_______________\\
+           \e[1;33m )_/  \e[1;31m    ;;; ┣╋╋╋╋┫ ;;;
+          \e[1;32m (@      \e[1;31m  ;;\e[1;32m ╲▂▂▂▂▂▂╱ \e[31m;;
+                      \e[1;31m                     "
+
+echo -e "                $green M $red S $purple F $reset - $blue A $red N $reset D $GreenF R $purple O            "
+echo ''
+banner
